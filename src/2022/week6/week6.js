9d62bdfacc9db931f277d2697532848c1c24d5b5
Danny Skoog <danny.skoog@live.se>
2022-02-07 16:04:11 +0100

    Add code challenge for week 6 of 2022

---
+++ src/2022/week6/week6.js
@@ -0,0 +1,24 @@
+/**
+* Code challenge week 6, 2022 (language/tool: Javascript, playground: https://jsfiddle.net/ with “Console” available at the bottom right corner)
+*
+* Description:
+*     The code below is supposed to populate an address object out of the address related properties of a person object. And then finishing
+*     off by logging the address object.
+*
+* Questions:
+*     1. What would you replace the question mark (?) with in order to populate the address object?
+*/
+
+const personObj = {
+    fullName: 'John Doe',
+    country: 'USA',
+    city: 'New York',
+    street: 'Main st',
+    postalCode: 12345,
+};
+
+let addressObj = {};
+
+// ?
+
+console.log(addressObj);
